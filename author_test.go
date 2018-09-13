aeb77127be7044b59cb8f17135670e75fea7f38d
mauleyzaola <mauricio.leyzaola@gmail.com>
2018-09-13 13:13:36 -0500

    #4 - author unit tests passing

---
+++ author_test.go
@@ -0,0 +1,14 @@
+package gitlog
+
+import "testing"
+
+func TestAuthor_TrimEmailChars(t *testing.T) {
+	author := &Author{
+		Name:  "mauleyzaola",
+		Email: "<mauricio.leyzaola@gmail.com>",
+	}
+	author.Email = author.TrimEmailChars()
+	if expected, actual := "mauricio.leyzaola@gmail.com", author.Email; expected != actual {
+		t.Errorf("expected:%v actual:%v", expected, actual)
+	}
+}
