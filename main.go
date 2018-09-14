2a8bb869e4d80ab48c09464c29a9f2727f56d701
mauleyzaola <mauricio.leyzaola@gmail.com>
2018-09-13 22:16:35 -0500

    #12 - json output from any directory

--- main.go
+++ main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+
+	"github.com/golang/glog"
+)
+
+func main() {
+	var directory string
+
+	flag.StringVar(&directory, "directory", "./.git", "the path to the the .git directory")
+	flag.Parse()
+
+	gitResult, err := RunGitLog(directory)
+	if err != nil {
+		glog.Exit(err)
+	}
+	commits, err := ParseCommitLines(gitResult)
+	if err != nil {
+		glog.Exit(err)
+	}
+	data, err := json.Marshal(&commits)
+	if err != nil {
+		glog.Exit(err)
+	}
+	fmt.Print(string(data))
+}
