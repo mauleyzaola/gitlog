5f149b97669a8c902cead1d2b40f99cd76c6f26b
mauleyzaola <mauricio.leyzaola@gmail.com>
2018-09-19 21:40:10 -0500

    disabled comment field in json output

--- commit.go
+++ commit.go
@@ -7,7 +7,7 @@ type Commit struct {
 	Hash    string    `json:"hash"`
 	Author  *Author   `json:"author"`
 	Date    time.Time `json:"date"` // for the sake of simplicity we deal only with AuthorDate
-	Comment string    `json:"comment"`
+	Comment string    `json:"-"`    // issues with JSON.parse(), need more rework
 	Added   int64     `json:"added"`
 	Deleted int64     `json:"removed"`
 }
